c85c8be89d0fad3c835094fb1de832fca01ce6e5
akshatlakhiwal1992 <163159774+akshatlakhiwal1992@users.noreply.github.com>
2025-11-05 11:52:33 -0500

    app.py

--- app.py
+++ app.py
@@ -359,7 +359,7 @@ def update_skills(active_cell, rows, selected, conf_type):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=1010)
+    app.run(debug=True, port=1010)
 
 
 
@@ -431,6 +431,7 @@ if __name__ == '__main__':
 
 
 # In[ ]:
+
 
 
 
